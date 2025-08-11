2739db4cceefb6b2e8e8afff71069a64b11f41f9
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-08-11 09:42:39 +0530

    Class 2

---
+++ Class 2/src/App.js
@@ -0,0 +1,16 @@
+import Movies from "./components/Movies";
+import CompA from "./PropsEx/CompA";
+import CompB from "./PropsEx/CompB";
+
+
+let App = ()=>{
+    return <div>
+        <h1>GM</h1>
+
+        <Movies/>
+        <CompA/>
+        <CompB/>
+    </div>
+}
+
+export default App;
