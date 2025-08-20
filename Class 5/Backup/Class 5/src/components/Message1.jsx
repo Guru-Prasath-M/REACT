7619a139497408a2784e9abf45d5c84d60b7241a
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-08-20 10:37:21 +0530

    React link Bootstrap

---
+++ Class 5/Backup/Class 5/src/components/Message1.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+class Message1 extends React.Component{
+    //msg="Hello.."
+    state={
+        "msg":"Hello,....."
+    }
+    gmHandler=()=>{
+        this.setState({"msg":"Good Morning"})
+    }
+    gnHandler=()=>{
+        this.setState({"msg":"Good Night"})
+    }
+    render(){
+        console.log("Inside Render Method")
+        return <div>
+                <h3>Message Component</h3>
+                <h3>Message Value:{this.state.msg}</h3>
+                <button onClick={this.gmHandler}>Gud Mrng</button>
+                <button onClick={this.gnHandler}>Gud Night</button>
+               
+        
+                </div>
+                
+    }
+}
+export default Message1;
+
+//Props - Sending one data from another...
+//State - Component own data...
