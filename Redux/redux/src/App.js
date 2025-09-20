8fff859ef4f6005b0a4c5fe06393c796f0ffb3b5
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-09-20 12:44:09 +0530

    Redux

--- Redux/redux/src/App.js
+++ Redux/redux/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import { Provider } from "react-redux";
+import Message from "./Message/Message";
+import { store } from "./Redux/store";
 
-const App = () => {
+
+function App() {
   return (
     <div>
-      <h1>App Component</h1>
+    <Provider store={store}>
+       <h1>App Components</h1>
+       <hr />
+    
+      <Message/>
+    </Provider>
     </div>
-  )
+  );
 }
 
 export default App;
